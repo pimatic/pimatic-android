4fcac7e031a70b9d3fbf6ac40b503fabec892b78
Oitzu <blackoise@googlemail.com>
2015-05-23 01:16:43 +0200

    Remove 'Settings' completely from menu.

--- app/src/main/java/org/pimatic/app/MainActivity.java
+++ app/src/main/java/org/pimatic/app/MainActivity.java
@@ -3,15 +3,8 @@ package org.pimatic.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import org.pimatic.connection.Connection;
 import org.pimatic.model.ConnectionOptions;
@@ -29,9 +22,6 @@ public class MainActivity extends AppCompatActivity
     private int activeGroupPosition;
     private int activeChildPosition;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
     private CharSequence mTitle;
 
     @Override
@@ -127,88 +117,6 @@ public class MainActivity extends AppCompatActivity
         outState.putInt("activeChildPosition", activeChildPosition);
 //        getSupportFragmentManager().putFragment(outState, "mSavedState", getSupportFragmentManager().findFragmentByTag(activeMainFragmentTag));
 
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.devices);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-       // actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        /*
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(MainActivity.this, SettingsFragment.class);
-            startActivity(intent);
-            return true;
-        }
-        */
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class DevicePageFragment extends Fragment {
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static DevicePageFragment newInstance(int sectionNumber) {
-            DevicePageFragment fragment = new DevicePageFragment();
-            Bundle args = new Bundle();
-            args.putInt("page_index", sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public DevicePageFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt("page_index"));
-        }
     }
 
 }
