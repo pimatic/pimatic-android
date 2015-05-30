888e313de3c199699129f70c7d13710695341605
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-05-30 11:40:00 +0200

    Crash reporter

---
+++ app/src/main/java/org/pimatic/app/PimaticApplication.java
@@ -0,0 +1,34 @@
+package org.pimatic.app;
+
+import android.app.Application;
+import android.content.Intent;
+
+/**
+ * Created by h3llfire on 30.05.15.
+ */
+public class PimaticApplication extends Application {
+    public void onCreate ()
+    {
+        // Setup handler for uncaught exceptions.
+        Thread.setDefaultUncaughtExceptionHandler (new Thread.UncaughtExceptionHandler()
+        {
+            @Override
+            public void uncaughtException (Thread thread, Throwable e)
+            {
+                handleUncaughtException (thread, e);
+            }
+        });
+    }
+
+    public void handleUncaughtException (Thread thread, Throwable e)
+    {
+        e.printStackTrace(); // not all Android versions will print the stack trace automatically
+
+        Intent intent = new Intent ();
+        intent.setAction ("com.mydomain.SEND_LOG"); // see step 5.
+        intent.setFlags (Intent.FLAG_ACTIVITY_NEW_TASK); // required when starting from Application
+        startActivity (intent);
+
+        System.exit(1); // kill off the crashed app
+    }
+}
